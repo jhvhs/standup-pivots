a368f34d1090d06cfb7f9af871f6ffb79b2849e4
Konstantin Semenov <ksemenov@pivotal.io>
2018-03-20 00:12:47 +0000

    Remove GAE specific endpoint

--- dublinstandup/views.py
+++ dublinstandup/views.py
@@ -11,14 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.template.response import SimpleTemplateResponse
-
-try:
-    from google.appengine.api import mail
-except ImportError:
-    import gmail_dummy as mail
 
 from .models import Schedule
 
@@ -43,31 +37,6 @@ def index(request):
         'schedule': current_schedule,
         'following_schedule': current_schedule.following_schedule,
     })
-
-
-def notify(request):
-    if request.META.get('HTTP_X_APPENGINE_CRON') != 'true':
-        raise PermissionDenied()
-
-    schedule = Schedule.next_schedule()
-    first_pivot = schedule.first_pivot
-    second_pivot = schedule.second_pivot
-
-    mail.send_mail(
-        sender=SENDER,
-        to="{} <{}>".format(first_pivot.full_name, first_pivot.email),
-        subject=SUBJECT,
-        body=MESSAGE.format(first_pivot.first_name, second_pivot.first_name)
-    )
-
-    mail.send_mail(
-        sender=SENDER,
-        to="{} <{}>".format(second_pivot.full_name, second_pivot.email),
-        subject=SUBJECT,
-        body=MESSAGE.format(second_pivot.first_name, first_pivot.first_name)
-    )
-
-    return HttpResponse("{} and {} have been notified".format(first_pivot, second_pivot))
 
 
 def slack_notification(request):
