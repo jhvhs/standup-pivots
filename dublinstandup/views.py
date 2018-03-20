d1b94c88d2a9eab9fabfb6e97e0f65715eed3eb5
Konstantin Semenov <ksemenov@pivotal.io>
2018-03-20 00:46:08 +0000

    Remove dead code

--- dublinstandup/views.py
+++ dublinstandup/views.py
@@ -1,32 +1,7 @@
-# Copyright 2015 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from django.http import HttpResponse
 from django.template.response import SimpleTemplateResponse
 
 from .models import Schedule
-
-SENDER = "reminder-bot@cf-pcf-kubo.appspotmail.com"
-SUBJECT = "You have been chosen to lead the standup next week"
-MESSAGE = """Hi {},
-
-You and {} have been chosen to run the standup next week.
-Please make sure to come to the office a bit earlier.
-
-Yours truly,
-
-The Dublin Standup Bot 
-                   """
 
 SLACK_MESSAGE = "The standup hosts for the next week are <@{}> and <@{}>"
 
